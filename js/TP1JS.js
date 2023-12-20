b48c140eb35662dc06bff6083698f0143d693a28
Alud24k <ifem.perez@gmail.com>
2023-12-20 00:44:21 -0300

    Se calculo grados celsius y fahrenheit con variables

--- js/TP1JS.js
+++ js/TP1JS.js
@@ -9,9 +9,20 @@ document.write("<br>El resultado de la multiplicacion es: " + impar * par);
 
 document.write("<br>El resultado de la division es: " + impar / par);
 
-let nombre = "Elias"
-let apellido = " Perez"
+let nombre = "Elias";
+let apellido = " Perez";
 
-let nombreCompleto = nombre + apellido
+let nombreCompleto = nombre + apellido;
 
-document.write("<br>El nombre completo es: "+ nombreCompleto)
+document.write("<br>El nombre completo es: " + nombreCompleto);
+
+gradosCelsius = 25;
+gradosFahrenheit = (gradosCelsius * 9) / 5 + 32;
+
+document.write(
+  "<br>" +
+    gradosCelsius +
+    "°C grados celsius son equivalentes a " +
+    gradosFahrenheit +
+    "°F grados farenheit"
+);
