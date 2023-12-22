268a6a82fca7501c3c2e686706d59339560b86f5
Alud24k <ifem.perez@gmail.com>
2023-12-22 14:42:32 -0300

    Se creo un programa donde pide dos numero e indica cual es el mayor

---
+++ js/06-ejercicio.js
@@ -0,0 +1,10 @@
+var numero1 = parseFloat(prompt("Ingresa el primer número:"));
+var numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+if (numero1 > numero2) {
+  alert("El " + numero1 + " es el número más grande.");
+} else if (numero2 > numero1) {
+  alert("El " + numero2 + " es el número más grande.");
+} else {
+  alert("Los números son iguales.");
+}
