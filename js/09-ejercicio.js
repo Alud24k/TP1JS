2db1578ebefcca482641fdb716e44f4d8dcbdbc1
Alud24k <ifem.perez@gmail.com>
2023-12-26 14:18:27 -0300

    Se creo un programa que muestra las vocales de la palabra que se ingresa

---
+++ js/09-ejercicio.js
@@ -0,0 +1,16 @@
+let palabra = prompt("Ingrese una palabra");
+let vocales = "";
+
+for (let index = 0; index < palabra.length; index++) {
+  const caracter = palabra.charAt(index).toLowerCase();
+  if (
+    caracter === "a" ||
+    caracter === "e" ||
+    caracter === "i" ||
+    caracter === "o" ||
+    caracter === "u"
+  ) {
+    vocales += caracter;
+  }
+}
+alert("Las vocales son: " + vocales);
